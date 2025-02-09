127998373f56f20a102c4bad1723823f096ea06c
Archithabajjuri <architha.bajjuri@gmail.com>
2025-02-09 16:48:06 +0530

    Add files via upload

---
+++ day27/conversions.py
@@ -0,0 +1,93 @@
+#conversions of int -flaot,str,bool,list,tuple,set,dict,range,int
+a=7
+print(float(a))
+print(int(a))
+print(str(a))
+print(bool(a))
+print(range(a))
+# print(set(a)) - from int to (set,list,dict,tuple ) convesrions is to possible
+# print(list(a))- it shows an error it possible.
+# print(dict(a))
+# print(tuple(a))
+#conversion of flaot-float ,int, str,bool,list,tuple,set,dict,range
+b=5.5
+print(float(b))
+print(int(b))
+print(str(b))
+print(bool(b))
+# print(list(b))-These conversions is not possible
+# print(tuple(b))
+# print(set(b))
+# print(dict(b))
+# print(range(b))
+c="1"  #conversion of string -string,int,float,bool,list,set,dict,range,tuple
+print(str(c))
+print(float(c))
+print(int(c))
+print(bool(c))
+print(list(c))
+print(tuple(c))
+print(set(c))
+# print(dict(c))-#-These conversions is not possible
+# print(range(c))
+d=True #conversion of bool-bool,string,int,float,list,set,dict,range,tuple
+print(bool(d))
+print(str(d))
+print(float(d))
+print(int(d))
+# print(list(d))-#-These conversions is not possible
+# print(tuple(d))#These conversions shows error
+# print(set(d))
+# print(dict(d))
+print(range(d))
+E=[1,5,7] #conversion of list-list,string,int,float,bool,set,dict,range,tuple
+print(str(E))
+# print(float(E)) #-These conversions is not possible
+# print(int(E))
+print(bool(E))
+print(list(E))
+print(tuple(E))
+print(set(E))
+# print(dict(E))  #-These conversions is not possible
+# print(range(E))
+F=(1.1, 2.2, 3.3,5,6) ##conversion of tuple- tuple,list,string,int,float,bool,set,dict,range,
+print(str(F))
+# print(float(F))
+# print(int(F))
+print(bool(F))
+print(list(F))
+print(tuple(F))
+print(set(F))
+# print(dict(F))#-These conversions is not possible
+# print(range(F))
+G={1,5,7} #conversion of set-set,string,int,float,bool,list,dict,range,tuple
+print(set(G))
+# print(int(G))
+# print(float(G))
+print(str(G))
+print(bool(G))
+print(list(G))
+# print(dict(G))
+# print(range(G))
+print(tuple(G))
+H={a:1,b:2,c:3,d:"hello"} #conversion of dict-dict,string,int,float,bool,set,list,range,tuple
+print(dict(H))
+# print(int(H))
+# print(float(H))
+print(str(H))
+print(bool(H))
+print(list(H))
+print(set(H))
+# print(range(H))
+print(tuple(H))
+
+range=(0,1,5,7,11) #conversion of range-range,string,int,float,bool,set,list,dict,tuple
+# print(dict(range))
+# print(int(range))
+# print(float(range))
+print(str(range))
+print(bool(range))
+print(list(range))
+print(set(range))
+print(range(range))
+print(tuple(range))
