9c2b0c84e823f433215ed194dec8b2204e88f767
Archithabajjuri <architha.bajjuri@gmail.com>
2025-02-12 18:08:50 +0530

    Add files via upload

---
+++ javasrcipt/Day3 task functions.py
@@ -0,0 +1,79 @@
+Question1
+function sayhello(){
+    console.log("Hello world")
+}
+sayhello()
+Question.2
+function displaywarning(){
+    console.log("warning! System overloaded")
+}
+displaywarning()
+Question.3
+function printstatus(){
+    console.log("System status:Active")
+}
+printstatus()
+Question.4
+function initialize(){
+    console.log("initialization Complete")
+    shutdown()
+}
+function shutdown(){
+    console.log("system shutting down")
+}
+initialize()
+Question.5
+function startprocess(){
+    console.log("process started")
+    endprocess()
+}
+function endprocess(){
+    console.log("process Ended")
+}
+startprocess()
+Question.6
+function greatuser(name){
+    console.log("Hello,[name]!")
+}
+greatuser(python)
+greatuser(java srcipt)
+greatuser(sita)
+Question.7
+function calculatesum(a,b){
+    console.log(a+b)
+}
+calculatesum(7,5)
+calculatesum(1,5)
+calculatesum(1,7)
+Question.8
+function countdown(){
+    for (var i = 5; i >= 0; i--) {
+        console.log(i);
+    }
+}
+countDown()
+Question.9
+function checkevenodd(){
+    if(num  % 2 == 0){
+        console.log("Even")
+    }
+    else{
+        console.log("Odd")
+    }
+}
+checkEvenOdd(8)
+checkEvenOdd(5)
+Question.10
+function beginsession(){
+    console.log("session started")
+    endsession()
+}
+function endsession(){
+    console.log("session Ended")
+}
+beginsession()
+
+
+
+
+
